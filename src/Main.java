8cbbed4444ca10560cf0d1fba79bd4dff64657bc
EvgenyKatukhov <ekatuhov@gmail.com>
2022-02-07 14:46:13 +0300

    miles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5000;
+        int count = 20;
+        int bonus = amount/count;
+        System.out.println("Ваш бонус "+ bonus +" миль");
+    }
+}
+
